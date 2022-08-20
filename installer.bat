0e8d30ddcdc36e479e4163b6b930936c93cc99d4
aiden04 <tingting2565@gmail.com>
2022-08-20 08:32:11 -0400

    Cleaned up Installer

--- installer.bat
+++ installer.bat
@@ -1,4 +1,5 @@
 @echo off
+title Spicetify-GUI Installer
 
 setlocal
 call :setESC
@@ -6,7 +7,7 @@ call :setESC
 echo.
 echo %ESC%[94m#####################################%ESC%[0m
 echo.
-echo %ESC%[94mSetting up Spicetify-GUI.%ESC%[0m
+echo %ESC%[94m [+] Setting up Spicetify-GUI.%ESC%[0m
 echo.
 echo %ESC%[94m#####################################%ESC%[0m
 timeout /t 1 > null
@@ -17,7 +18,7 @@ cls
 echo.
 echo %ESC%[94m#####################################%ESC%[0m
 echo.
-echo %ESC%[94mSetting up Spicetify-GUI. .%ESC%[0m
+echo %ESC%[94m [+] Setting up Spicetify-GUI. .%ESC%[0m
 echo.
 echo %ESC%[94m#####################################%ESC%[0m
 timeout /t 1 >  null
@@ -28,7 +29,7 @@ cls
 echo.
 echo %ESC%[94m#####################################%ESC%[0m
 echo.
-echo %ESC%[94mSetting up Spicetify-GUI. . .%ESC%[0m
+echo %ESC%[94m [+] Setting up Spicetify-GUI. . .%ESC%[0m
 echo.
 echo %ESC%[94m#####################################%ESC%[0m
 timeout /t 1 > null
@@ -39,14 +40,18 @@ cls
 echo.
 echo %ESC%[94m#####################################%ESC%[0m
 echo.
-echo %ESC%[94mUpdating PIP%ESC%[0m
+echo %ESC%[94m [+] Updating PIP%ESC%[0m
 echo.
 echo %ESC%[94m#####################################%ESC%[0m
-echo %ESC%[94mStarting Update%ESC%[0m
+echo.
+echo %ESC%[94m [-] Starting Update%ESC%[0m
+echo.
+color 0b
 pip install --upgrade pip
+echo.
 echo %ESC%[32m#####################################%ESC%[0m
 echo.
-echo %ESC%[32mPip Updated!%ESC%[0m
+echo %ESC%[32m [+] Pip Updated!%ESC%[0m
 echo.
 echo %ESC%[32m#####################################%ESC%[0m
 timeout /t 2 > null
@@ -56,15 +61,18 @@ goto requirements
 cls
 echo %ESC%[94m#####################################%ESC%[0m
 echo.
-echo %ESC%[94mInstalling Pip Requirements%ESC%[0m
+echo %ESC%[94m [+] Installing Pip Requirements%ESC%[0m
 echo.
 echo %ESC%[94m#####################################%ESC%[0m
 echo.
-echo %ESC%[94mInstalling requirements.txt%ESC%[0m
+echo %ESC%[94m [-] Installing requirements.txt%ESC%[0m
+echo.
+color 0b
 pip install -r requirements.txt
+echo.
 echo %ESC%[32m#####################################%ESC%[0m
 echo.
-echo %ESC%[32mRequirements Installed!%ESC%[0m
+echo %ESC%[32m [+] Requirements Installed!%ESC%[0m
 echo.
 echo %ESC%[32m#####################################%ESC%[0m
 timeout /t 2 > null
@@ -74,15 +82,18 @@ goto PyInstaller
 cls
 echo %ESC%[94m#####################################%ESC%[0m
 echo.
-echo %ESC%[94mCreating Executable with PySintaller%ESC%[0m
+echo %ESC%[94m [+] Creating Executable with PySintaller%ESC%[0m
 echo.
 echo %ESC%[94m#####################################%ESC%[0m
 echo.
-echo %ESC%[94mCreating Executable%ESC%[0m
+echo %ESC%[94m [-] Creating Executable%ESC%[0m
+echo.
+color 0b
 pyinstaller --onefile --windowed --name Spicetify-GUI --noupx -i "src/spicetify-logo.ico" --distpath "spicetify-gui" "spicetify.pyw" --clean
+echo.
 echo %ESC%[32m#####################################%ESC%[0m
 echo.
-echo %ESC%[32mExecutable Created!%ESC%[0m
+echo %ESC%[32m [+] Executable Created!%ESC%[0m
 echo.
 echo %ESC%[32m#####################################%ESC%[0m
 timeout /t 3 > null
@@ -92,33 +103,38 @@ goto clean_up
 cls
 echo %ESC%[94m#####################################%ESC%[0m
 echo.
-echo %ESC%[94mCleaning Up%ESC%[0m
+echo %ESC%[94m [+] Cleaning Up%ESC%[0m
 echo.
 echo %ESC%[94m#####################################%ESC%[0m
 echo.
+echo %ESC%[94m [-] move spicetify-gui\spicetify-gui.exe .\spicetify-gui.exe[0m
 move spicetify-gui\spicetify-gui.exe .\spicetify-gui.exe
+echo %ESC%[94m [-] RMDIR /Q/S build
 RMDIR /Q/S build
 timeout /t 1 >null
+echo %ESC%[94m [-] RMDIR /Q/S spicetify-gui
 RMDIR /Q/S spicetify-gui
 timeout /t 1 >null
+echo %ESC%[94m [-] del /f Spicetify-GUI.spec
+timeout /t 3 >null
+echo.
 del /f Spicetify-GUI.spec
-timeout /t 1 >null
-echo %ESC%[32m#####################################%ESC%[0m
+echo %ESC%[32m#####################################%ESC%
 echo.
-echo %ESC%[32mDirectory cleaned up!%ESC%[0m
+echo %ESC%[32m [+] Directory cleaned up!%ESC%
 echo.
-echo %ESC%[32m#####################################%ESC%[0m
+echo %ESC%[32m#####################################%ESC%
 timeout /t 3 > null
 
 :completed
 cls
 echo %ESC%[32m#####################################%ESC%[0m
 echo.
-echo %ESC%[32mInstallation Completed!%ESC%[0m
+echo %ESC%[32m [+]Installation Completed!%ESC%[0m
 echo.
 echo %ESC%[32m#####################################%ESC%[0m
 echo.
-echo %ESC%[32mPress any key to exit. . .%ESC%[0m
+echo %ESC%[32m [-]Press any key to exit. . .%ESC%[0m
 pause > null
 
 
